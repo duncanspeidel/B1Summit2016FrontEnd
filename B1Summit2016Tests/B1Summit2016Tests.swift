09fb9d1fda87a6b3ce40f679b43472d0e32ba235
Li Yatsea <i033357@melm50899999a.dhcp.mel.sap.corp>
2015-09-30 18:11:21 +1000

    Initial Commit

---
+++ B1Summit2016Tests/B1Summit2016Tests.swift
@@ -0,0 +1,36 @@
+//
+//  B1Summit2016Tests.swift
+//  B1Summit2016Tests
+//
+//  Created by Li, Yatsea on 9/30/15.
+//  Copyright © 2015 Li, Yatsea. All rights reserved.
+//
+
+import XCTest
+@testable import B1Summit2016
+
+class B1Summit2016Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
